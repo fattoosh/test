ab7a74b79e05664acc5712daa8798e58fa4a9036
Omar Fityan <omarfityan@yahoo.com>
2018-10-02 18:20:13 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Omar Fityan on 10/2/18.
+//  Copyright © 2018 Omar Fityan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
+    }
+
+    @IBAction func button(_ sender: Any) {
+        if let name = textField.text {
+            label.text = "Hi \(name)!"
+        }
+        textField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
